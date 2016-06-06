3658a772a4345a4b1f60dc17de04940bac278a8a
Michael Codner <mhcodner@gmail.com>
2016-06-06 21:30:29 +1000

    fix condition

--- advassignment.c
+++ advassignment.c
@@ -60,7 +60,7 @@ int main (int argc, char* argv[]) {
         for (j = 0; argv[i][j] != '\0'; j++) { /* Loop over each character in each word */
             current->c = argv[i][j]; /* Add the character to the linked list */
             current->next = malloc(sizeof(node_t)); /* Allocate memory for the next item */
-            if (argv[i][j+1] != '\0')
+            if (argv[i][j+1] != '\0' || i < argc - 1)
                 current = current->next; /* Set current to the pointer for the next item so that we continue
                                           * adding to the list instead of changing the same item */
         }
